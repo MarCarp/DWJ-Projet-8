35c20aa018da4702155149162710c3d0de98a402
Marco <marcarp@protonmail.com>
2018-02-28 15:55:46 +0100

    Ajouté class Post dans Model/Post.php

---
+++ Model/Post.php
@@ -0,0 +1,17 @@
+<?php
+class Post extends Model
+{
+	public function getPosts()
+	{
+		$sql = 'SELECT POST_TITLE title, POST_CONTENT content, FORMAT_DATE(POST_DATE, "%M %j, $Y/") datefr FROM blg_posts ORDER BY POST_ID DESC';
+		$posts = $this->request($sql);
+		return $posts;
+	}
+
+	public function getPost($postId)
+	{
+		$sql = 'SELECT POST_TITLE title, POST_CONTENT content, FORMAT_DATE(POST_DATE, "%M %j, $Y/") datefr FROM blg_posts WHERE POST_ID=?';
+		$post = $this->request($sql,array($postId));
+		return $post;		
+	}
+}
