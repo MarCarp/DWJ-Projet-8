753d83cf748eac343f037dc1e5bab7d3342a1192
Marco <marcarp@protonmail.com>
2018-03-02 16:38:00 +0100

    Correction affichage date

--- Model/Post.php
+++ Model/Post.php
@@ -4,14 +4,14 @@ class Post extends Model
 {
 	public function getPosts()
 	{
-		$sql = 'SELECT POST_ID id, POST_TITLE title, POST_CONTENT content, POST_IMG image, DATE_FORMAT(POST_DATE, "%b %e, %Y/") datefr FROM blg_posts ORDER BY POST_ID DESC';
+		$sql = 'SELECT POST_ID id, POST_TITLE title, POST_CONTENT content, POST_IMG image, DATE_FORMAT(POST_DATE, "%b %e, %Y") datefr FROM blg_posts ORDER BY POST_ID DESC';
 		$posts = $this->request($sql);
 		return $posts;
 	}
 
 	public function getPost($postId)
 	{
-		$sql = 'SELECT POST_TITLE title, POST_CONTENT content, POST_IMG image, DATE_FORMAT(POST_DATE, "%b %e, %Y/") datefr FROM blg_posts WHERE POST_ID=?';
+		$sql = 'SELECT POST_TITLE title, POST_CONTENT content, POST_IMG image, DATE_FORMAT(POST_DATE, "%b %e, %Y") datefr FROM blg_posts WHERE POST_ID=?';
 		$post = $this->request($sql, array($postId));
 		return $post->fetch();		
 	}
